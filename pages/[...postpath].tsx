bdba176131f3fadd87c35a6e1eec5c61d7eb34ba
TBG120 <116538827+TBG120@users.noreply.github.com>
2026-05-21 21:38:53 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-import { GraphQLClient, gql } from 'graphql-request'; 'https://moviebuzzamerica.byethost17.com/graphql'
+import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = process.env.GRAPHQL_ENDPOINT as string;'https://moviebuzzamerica.byethost17.com/graphql'
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://moviebuzzamerica.byethost17.com` + encodeURI(path as string)
+					`https://moviebuzzamerica.byethost17.com/` + encodeURI(path as string)
 				}`,
 			},
 		};

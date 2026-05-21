45cc90702011b34ae44b0758be6cb597b85f031a
TBG120 <116538827+TBG120@users.noreply.github.com>
2026-05-21 21:31:08 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://moviebuzzamerica.byethost17.com/graphql` + encodeURI(path as string)
+					`https://moviebuzzamerica.byethost17.com` + encodeURI(path as string)
 				}`,
 			},
 		};
